70e75812d82e14c2bf909fedc949414702e4d667
JeremicAleks <jeremicaleksandar96@gmail.com>
2020-03-23 21:58:50 +0100

    init SpringBoot project

---
+++ ScientificCenter/src/main/java/com/elastic/scientificenterelastic/ScientificenterelasticApplication.java
@@ -0,0 +1,13 @@
+package com.elastic.scientificenterelastic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScientificenterelasticApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScientificenterelasticApplication.class, args);
+    }
+
+}
---
+++ ScientificCenter/src/test/java/com/elastic/scientificenterelastic/ScientificenterelasticApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elastic.scientificenterelastic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScientificenterelasticApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
